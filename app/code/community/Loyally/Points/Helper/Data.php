bff4566210fed5ef07e350f2deb75c1bb6116068
Mark Strefford <markstrefford@me.com>
2012-07-14 20:31:09 +0100

    Data.php and xml files added

---
+++ app/code/community/Loyally/Points/Helper/Data.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This source file is (c) 2012 Loyally.me
+ * 
+ * @category    Loyally
+ * @package     Loyally_Points
+ * @copyright   Copyright (c) 2012 Loyally.me
+ * @license     http://loyally.me
+ *
+ * 
+ * *** THIS FILE IS FOR ENROLING A USER IN A SCHEME IF THEY ARE NOT ALREADY ENROLED ****
+ * 
+ * 
+ */
+ 
+// *** Need to work out what a helper abstract class is!!
+class Loyally_LoyallyPoints_Helper_Data extends Mage_Core_Helper_Abstract
+{
+ 	// Check whether the config setting for points is enabled
+  	public function isLoyallyEnabled()
+  	{
+    	if(Mage::getStoreConfig('points/settings/enabled') == 0)
+    	  return FALSE;
+    	else
+    	  return TRUE;
+  	}
+
+	// Do we need to do anything else here to set loyally up?
+}
